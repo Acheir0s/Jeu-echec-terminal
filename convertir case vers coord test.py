9a044f02af2207c4c24eb4a76f28adfa25e9365f
Acheir0s <poultierdamien@gmail.com>
2025-02-10 10:35:53 +0100

    Delete convertir case vers coord test.py

--- convertir case vers coord test.py
+++
@@ -1,7 +0,0 @@
-def convertir_case_vers_coord(case):
-        lettre, chiffre = case
-        y= int(chiffre)  # Inverse la notation des lignes, car la ligne 8 est la première
-        x = ord(lettre) - ord('a')+1  # Convertie la lettre en index (a -> 0, b -> 1, etc.)
-        return y,x 
-
-print(convertir_case_vers_coord('a2'))
